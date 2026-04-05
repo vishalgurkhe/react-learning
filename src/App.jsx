111e138c841bace457c2e822f364fe87f21ea4d1
Vishal <gurkhevishal645@gmail.com>
2026-04-05 10:26:18 +0530

    Refactored the code

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,18 @@
 import { useState } from 'react'
 import './App.css'
 import Counter from './components/Counter'
+import InputOnChange from './components/InputOnChange'
 
 function App() {
 
-  // const [val,setVal]=useState("Vishal");
-  const [val1,setVal1]=useState("");
-  const [val2,setVal2]=useState("");
-  const [val3,setVal3]=useState("");
+
 
   return (
     <>
       {/* <Counter /> */}
 
-      {/* <h1>Enter the input</h1>
-      <input type='text' value={val} 
-              onChange={(event)=>{setVal(event.target.value)}}
-      ></input>
-      <p>{val}</p>
-
-      <button onClick={()=>setVal("")}>Clear </button> */}
-
-      <input type="text" value={val1}
-            onChange={(event)=>{setVal1(event.target.value)}}></input>
-      <input type="text" value={val2}
-            onChange={(event)=>{setVal2(event.target.value)}}></input>
-      <input type="text" value={val3}
-            onChange={(event)=>{setVal3(event.target.value)}}></input>
-
-      <button onClick={()=>console.log(val1+val2+val3)}>Show Values</button>
-
+<InputOnChange />
+  
     </>
   )
 }
---
+++ src/components/InputOnChange.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+function InputOnChange() {
+      // const [val,setVal]=useState("Vishal");
+ const [val1,setVal1]=useState("");
+  const [val2,setVal2]=useState("");
+  const [val3,setVal3]=useState("");
+
+  return (
+    <div>
+
+    {/* <h1>Enter the input</h1>
+      <input type='text' value={val} 
+              onChange={(event)=>{setVal(event.target.value)}}
+      ></input>
+      <p>{val}</p>
+
+      <button onClick={()=>setVal("")}>Clear </button> */}
+
+        <input type="text" value={val1}
+            onChange={(event)=>{setVal1(event.target.value)}}></input>
+      <input type="text" value={val2}
+            onChange={(event)=>{setVal2(event.target.value)}}></input>
+      <input type="text" value={val3}
+            onChange={(event)=>{setVal3(event.target.value)}}></input>
+
+      <button onClick={()=>console.log(val1+val2+val3)}>Show Values</button>
+
+      
+      
+    </div>
+  )
+}
+
+export default InputOnChange;
